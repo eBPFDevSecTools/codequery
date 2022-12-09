c55b948da7f1d9135fed260854b2080b271db79c
Sayandeep Sen <sayandes@in.ibm.com>
2022-12-09 09:02:24 +0000

    fixed bug

--- cli/main_cli.cpp
+++ cli/main_cli.cpp
@@ -261,8 +261,6 @@ int create_callee_tree_rec(tStr sqfn, tStr term, int intParam,
 		lstr = limitcstr(limitlen, it->linetext);
 		if(resultlst.result_type == sqlqueryresultlist::sqlresultFULL)
 		{
-			if(set_of_seen_fn.find(it->symname.c_str()) != set_of_seen_fn.end())
-				continue;
 			std::string fn_name_str(it->symname.c_str());
 			std::string map_line(lstr.c_str());
 			find_map_ref(fn_name_str, map_line, it->filepath+","+it->linenum);
@@ -272,7 +270,8 @@ int create_callee_tree_rec(tStr sqfn, tStr term, int intParam,
 				it->symname.c_str(),
 				(full ? it->filepath.c_str() : it->filename.c_str()),
 				it->linenum.c_str());*/
-			create_callee_tree_rec(sqfn, it->symname.c_str(), intParam, exact, depth +1, fpath, full, debug, limitlen, sq);
+			if(set_of_seen_fn.find(it->symname.c_str()) == set_of_seen_fn.end())
+				create_callee_tree_rec(sqfn, it->symname.c_str(), intParam, exact, depth +1, fpath, full, debug, limitlen, sq);
 		}
 	}
 	make_fn_defn_entry(term, exact, depth, fpath, full, debug, limitlen, sq);
